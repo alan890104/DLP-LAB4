5934a02fe9872c87993562d1dcf934b40e19a2e2
alan890104 <alan890104@gmail.com>
2023-08-15 14:12:46 +0800

    fisrt commit

---
+++ utils.py
@@ -0,0 +1,18 @@
+import torch
+import numpy as np
+import random
+
+
+def set_seed(seed: int = 890104):
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+
+
+def seed_worker(worker_id):
+    worker_seed = torch.initial_seed() % 2**32
+    np.random.seed(worker_seed)
+    random.seed(worker_seed)
